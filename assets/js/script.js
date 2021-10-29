a2a0f1d2429ab717d0db7e7002fd991a3ae696bb
scorpion77777 <vvmartirosyan@gmail.com>
2021-10-28 19:40:50 -0700

    check this out guys2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,12 @@ var api_key = "L0OJs7oDz2zUWKWzCpYFwYuggr5eEzlOjgTK4Pxg";
 req.open("GET", url + api_key);
 req.send();
 
+// Getting picture of the day data from API
 req.addEventListener("load", function () {
   if (req.status == 200 && req.readyState == 4) {
     var response = JSON.parse(req.responseText);
     document.getElementById("title").textContent = response.title;
+    console.log(title);
     document.getElementById("date").textContent = response.date;
     document.getElementById("pic").src = response.hdurl;
     document.getElementById("explanation").textContent = response.explanation;
