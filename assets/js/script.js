ccfacea1d0acdb6fdc74d50d93a22f5865e7b5f2
Dustin Smith <dustinsmith1983@gmail.com>
2021-11-05 13:59:26 -0700

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,18 @@ console.log(camId);
 getPic();
 
 $(function() {
-            $( "#my_date_picker" ).datepicker({
-                defaultDate:"2015-09-05",
-                dateFormat: "yy-mm-dd",
-                onSelect: function (evt, ui) {
-                    dayObj = ui;
-                    console.log(ui.selectedDay, ui.selectedMonth, ui.selectedYear)
-                    dateSel = `${ui.selectedYear}-${ui.selectedMonth+1}-${ui.selectedDay}`
-                    getPic()
-                }
+    $( "#my_date_picker" ).datepicker({
+        defaultDate:"2015-09-05",
+        dateFormat: "yy-mm-dd",
+        onSelect: function (evt, ui) {
+            dayObj = ui;
+            console.log(ui.selectedDay, ui.selectedMonth, ui.selectedYear)
+            dateSel = `${ui.selectedYear}-${ui.selectedMonth+1}-${ui.selectedDay}`
+            getPic()
+            }
             
-            });
-        });
+    });
+});
 
 function handleSelectChange(e){
     console.log(e.target.value);
@@ -40,42 +40,33 @@ function handlePhotoSelect(e){
 
 
 function getPic() {
-var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + dateSel + "&camera=" + camId +"&api_key=ueGl04f2AeAlKgT7JOzMv1onJoTXoF1zqrBidAQB";
+    var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + dateSel + "&camera=" + camId +"&api_key=ueGl04f2AeAlKgT7JOzMv1onJoTXoF1zqrBidAQB";
         fetch(url)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                 appendData(data);
+                appendData(data);
                 photos = data.photos;
             })
             .catch(function (err) {
-                 console.log('error: ' + err);
-             });
+            console.log('error: ' + err);
+        });
 
-            //Show data
+        //Show data
         function appendData(data) {
             console.log(data)
             console.log(data.photos.length);
             document.querySelector("#photoSelect").setAttribute("placeholder", `Please enter a number between 1 and ${data.photos.length}`)
             photoCount = data.photos.length;
-            // var response = JSON.parse(req.responseText);
             var mainContainer = document.getElementById("title");
             document.querySelector("#img").setAttribute("src", data.photos[1].img_src)
             mainContainer.textContent = data.title;
-
             //set limit on number input
             photoSelect.setAttribute("max", data.photos.length);
             photoSelect.setAttribute("min", 1);
-            // for (var i = 0; i < data.length; i++) {
-
-
-            // document.getElementById("title").textContent = response.title;
-            // mainContainer.appendChild(div);
-            // }
-
         }
-    }
+}
 
 
 select.addEventListener("change", handleSelectChange);
