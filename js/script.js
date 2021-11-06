fd2b19e48e7e1d755e9482548f1565697286b54a
Dustin Smith <dustinsmith1983@gmail.com>
2021-11-05 18:39:21 -0700

    modified api call to include dynamic rover selection, and added proper camera options to the correct rover pages

--- js/script.js
+++ js/script.js
@@ -5,24 +5,28 @@ var select = document.querySelector("#cameraId");
 var photoSelect = document.querySelector("#photoSelect");
 var photos = [];
 var dateSel = "2015-09-05"
-
-
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log( page );
+var path2 = page.split(".");
+var rover = path2[0];
+console.log(rover);
 console.log(camId);
 getPic();
 
 $(function() {
-            $( "#my_date_picker" ).datepicker({
-                defaultDate:"2015-09-05",
-                dateFormat: "yy-mm-dd",
-                onSelect: function (evt, ui) {
-                    dayObj = ui;
-                    console.log(ui.selectedDay, ui.selectedMonth, ui.selectedYear)
-                    dateSel = `${ui.selectedYear}-${ui.selectedMonth+1}-${ui.selectedDay}`
-                    getPic()
-                }
+    $( "#my_date_picker" ).datepicker({
+        defaultDate:"2015-09-05",
+        dateFormat: "yy-mm-dd",
+        onSelect: function (evt, ui) {
+            dayObj = ui;
+            console.log(ui.selectedDay, ui.selectedMonth, ui.selectedYear)
+            dateSel = `${ui.selectedYear}-${ui.selectedMonth+1}-${ui.selectedDay}`
+             getPic()
+        }
             
-            });
-        });
+    });
+});
 
 function handleSelectChange(e){
     console.log(e.target.value);
@@ -40,7 +44,7 @@ function handlePhotoSelect(e){
 
 
 function getPic() {
-var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + dateSel + "&camera=" + camId +"&api_key=ueGl04f2AeAlKgT7JOzMv1onJoTXoF1zqrBidAQB";
+    var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + dateSel + "&camera=" + camId +"&api_key=ueGl04f2AeAlKgT7JOzMv1onJoTXoF1zqrBidAQB";
         fetch(url)
             .then(function (response) {
                 return response.json();
@@ -52,30 +56,22 @@ var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth
             .catch(function (err) {
                  console.log('error: ' + err);
              });
+console.log(url);
+    //Show data
+    function appendData(data) {
+        console.log(data)
+        console.log(data.photos.length);
+        document.querySelector("#photoSelect").setAttribute("placeholder", `Please enter a number between 1 and ${data.photos.length}`)
+        photoCount = data.photos.length;
+        var mainContainer = document.getElementById("title");
+        document.querySelector("#img").setAttribute("src", data.photos[1].img_src)
+        mainContainer.textContent = data.title;
 
-            //Show data
-        function appendData(data) {
-            console.log(data)
-            console.log(data.photos.length);
-            document.querySelector("#photoSelect").setAttribute("placeholder", `Please enter a number between 1 and ${data.photos.length}`)
-            photoCount = data.photos.length;
-            // var response = JSON.parse(req.responseText);
-            var mainContainer = document.getElementById("title");
-            document.querySelector("#img").setAttribute("src", data.photos[1].img_src)
-            mainContainer.textContent = data.title;
-
-            //set limit on number input
-            photoSelect.setAttribute("max", data.photos.length);
-            photoSelect.setAttribute("min", 1);
-            // for (var i = 0; i < data.length; i++) {
-
-
-            // document.getElementById("title").textContent = response.title;
-            // mainContainer.appendChild(div);
-            // }
-
-        }
+        //set limit on number input
+        photoSelect.setAttribute("max", data.photos.length);
+        photoSelect.setAttribute("min", 1);
     }
+}
 
 
 select.addEventListener("change", handleSelectChange);
